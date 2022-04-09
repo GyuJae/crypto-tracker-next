3da4ecbbf4923dacb5cf2aabd4461dc166d59ed3
Gyujae <wjdrbwo1206@naver.com>
2022-04-09 17:50:41 +0900

    fixed

--- pages/drag/index.tsx
+++ pages/drag/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import { useEffect, useState } from "react";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import "@atlaskit/css-reset";
 
 const Index: NextPage = () => {
   const [winReady, setwinReady] = useState<boolean>(false);
@@ -8,57 +9,35 @@ const Index: NextPage = () => {
     setwinReady(true);
   }, []);
 
-  const grid = 8;
-  const getListStyle = (isDraggingOver: boolean) => ({
-    background: isDraggingOver ? "lightblue" : "lightgrey",
-    padding: grid,
-    width: 250,
-  });
-  const getItemStyle = (isDragging: boolean, draggableStyle: any) => ({
-    // some basic styles to make the items look a bit nicer
-    userSelect: "none",
-    padding: grid * 2,
-    margin: `0 0 ${grid}px 0`,
-
-    // change background colour if dragging
-    background: isDragging ? "lightgreen" : "grey",
-
-    // styles we need to apply on draggables
-    ...draggableStyle,
-  });
   const onDragEnd = () => {};
   return (
     <div>
       {winReady ? (
         <DragDropContext onDragEnd={onDragEnd}>
-          <Droppable droppableId="droppable">
-            {(provided, snapshot) => (
-              <div
-                {...provided.droppableProps}
-                ref={provided.innerRef}
-                style={getListStyle(snapshot.isDraggingOver)}
-              >
-                {[1, 2, 3, 4, 5, 6].map((item, index) => (
-                  <Draggable key={item} draggableId={item + ""} index={index}>
-                    {(provided, snapshot) => (
-                      <div
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                        style={getItemStyle(
-                          snapshot.isDragging,
-                          provided.draggableProps.style
-                        )}
-                      >
-                        {item}
-                      </div>
+          <div>
+            <Droppable droppableId="one">
+              {(magic) => (
+                <ul ref={magic.innerRef} {...magic.droppableProps}>
+                  <Draggable draggableId="first" index={0}>
+                    {(magic) => (
+                      <li ref={magic.innerRef} {...magic.draggableProps}>
+                        <span {...magic.dragHandleProps}>🔥</span>
+                        One
+                      </li>
                     )}
                   </Draggable>
-                ))}
-                {provided.placeholder}
-              </div>
-            )}
-          </Droppable>
+                  <Draggable draggableId="second" index={1}>
+                    {(magic) => (
+                      <li ref={magic.innerRef} {...magic.draggableProps}>
+                        <span {...magic.dragHandleProps}>🔥</span>
+                        Second
+                      </li>
+                    )}
+                  </Draggable>
+                </ul>
+              )}
+            </Droppable>
+          </div>
         </DragDropContext>
       ) : null}
     </div>
